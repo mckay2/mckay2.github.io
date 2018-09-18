3dce88e67f22d0440f111b886331a1e344ce1b0a
mmckay99 <michael_mac_i@me.com>
2018-09-18 17:52:46 +0100

    Updates

--- mckay2/.cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/michael/mckay2.github.io/mckay2/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/Users/michael/mckay2.github.io/mckay2/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
