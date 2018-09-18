78ff00c91530bce983c7a39a4b5d0f116e3e3bb9
mmckay99 <michael_mac_i@me.com>
2018-09-18 17:47:55 +0100

    stuff

--- mckay2/src/components/layout.jsx
+++ mckay2/src/components/layout.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import ReactHelmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import ReactHelmet from "react-helmet";
+import { StaticQuery, graphql } from "gatsby";
 
-import Header from './header/header'
-import './layout.css'
-import '../corestyles.module.css'
+import Header from "./header/header";
+
+import "../corestyles.module.css";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -23,8 +23,8 @@ const Layout = ({ children }) => (
         <ReactHelmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: "description", content: "Sample" },
+            { name: "keywords", content: "sample, something" }
           ]}
         >
           <html lang="en" />
@@ -32,10 +32,10 @@ const Layout = ({ children }) => (
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: '0 auto',
+            margin: "0 auto",
             maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
+            padding: "0px 1.0875rem 1.45rem",
+            paddingTop: 0
           }}
         >
           {children}
@@ -43,10 +43,10 @@ const Layout = ({ children }) => (
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
